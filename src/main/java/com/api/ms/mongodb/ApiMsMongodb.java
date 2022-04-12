91ef48fa5f5f03731d08051d4e2f98710bf076cc
Euller Henrique <eullerhenrique@outlook.com>
2022-04-12 09:56:02 -0300

    Update

--- src/main/java/com/api/ms/mongodb/ApiMsMongodb.java
+++ src/main/java/com/api/ms/mongodb/ApiMsMongodb.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectMongoDbDockerApplication {
+public class ApiMsMongodb {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjectMongoDbDockerApplication.class, args);
+        SpringApplication.run(ApiMsMongodb.class, args);
     }
 
 }
